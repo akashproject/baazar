5eda33ff00cc1c06eb300f82a3980d59d91b1848
Akash Dutta <akashdutta.scriptcrown@gmail.com>
2024-06-19 01:25:20 +0530

    update 2024

--- src/app/components/todays-upcoming-session/todays-upcoming-session.component.ts
+++ src/app/components/todays-upcoming-session/todays-upcoming-session.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UtilService } from '../../services/utils.service';
+import { ApiService } from '../../services/api.service';
+import { ToastrService } from 'ngx-toastr';
+import { environment } from '../../../environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-todays-upcoming-session',
@@ -6,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrl: './todays-upcoming-session.component.scss'
 })
 export class TodaysUpcomingSessionComponent {
+  public sessions : any = [];
+  mediaURL: any = '';
+  constructor(
+    private router: Router,
+    private api: ApiService,
+    public util: UtilService,
+    private toastr: ToastrService) {
+      this.mediaURL = environment.mediaURL;
+    }
 
+    ngOnInit(): void {
+      this.getLiveSessions();
+    }
+  
+    getLiveSessions(){
+      this.api.get("upcoming-sessions").subscribe((data: any) => {
+        this.sessions = data;
+      });
+    }
 }
