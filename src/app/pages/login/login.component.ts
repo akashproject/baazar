151af01e2fddccd9be21f79486308dd8055e5d82
akashproject <akashdutta.scriptcrown@gmail.com>
2024-06-19 19:27:09 +0530

    update 2024

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UtilService } from '../../services/utils.service';
+import { ApiService } from '../../services/api.service';
+import { SigninService } from '../../services/signin.service';
+import { ToastrService } from 'ngx-toastr';
+import { environment } from '../../../environments/environment';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-login',  
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  mediaURL: any = '';
+  token_data: any;
+  access_token: any = "";
+  token: string | undefined;
+  loginForm: FormGroup | any;
+
+  constructor(
+    private router: Router,
+    private api: ApiService,
+    private fb: FormBuilder,
+    private signinservice: SigninService,
+    public util: UtilService,
+    private toastr: ToastrService) {
+      this.mediaURL = environment.mediaURL;
+      this.loginForm = this.fb.group({
+        mobile: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]], // Validate 10-digit mobile number
+        password: ['', [Validators.required, Validators.minLength(8)]]
+      });
+    }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const mobile = this.loginForm.get('mobile')?.value;
+      const password = this.loginForm.get('password')?.value;
+      // Implement your login logic here
+      console.log('Mobile:', mobile);
+      console.log('Password:', password);
+      this.signinservice.signinUser(this.loginForm).subscribe(data => {
+        this.signinservice.setToken(data);
+        this.token_data = data;
+        this.token = this.token_data.access_token;
+        console.log(this.token);
+  
+        this.router.navigate(['/home']);
+  
+      }, (err: any) => {
+        // err = err
+        console.log(err);
+        // this.displayToastFailure();
+      });
+
+    } else {
+      // Handle form errors
+      console.log('Form is invalid');
+    }
+  }
 
 }
---
+++ src/app/services/signin.service.ts
@@ -0,0 +1,89 @@
+import {Inject, Injectable } from '@angular/core';
+import { UtilService } from './utils.service';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, from, Observable } from 'rxjs';
+import { switchMap, tap, map} from 'rxjs/operators/index';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SigninService {
+
+  JWT_KEY = 'access_token';
+
+  // localStorage: Storage;
+
+  private user = new BehaviorSubject(null);
+  constructor(private http: HttpClient) { }
+
+    signinUser(signinUser : {}) {
+      return this.http.post(`${environment.apiUrl}/login`, { signinUser });
+    }
+  
+    getCurrentUser() {
+      return localStorage.getItem(this.JWT_KEY) || null;
+    }
+  
+    logout(){
+      localStorage.removeItem(this.JWT_KEY);
+      localStorage.removeItem("login");
+      this.user.next(null);
+    }
+  
+    setToken(data : any){
+      localStorage.setItem(this.JWT_KEY, data);
+    }
+  
+    setFlag(key:any, value:any){
+      localStorage.setItem(key, value);
+    }
+  
+    getFlag(key:any){
+      return localStorage.getItem(key) || null;
+    }
+
+  
+    updateUser(name:string, mobile:string, homestate:string, gender:string, cast:string, city:string, physical_status:string, email:string, neet:string, access_token:string){
+  
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${access_token}`
+      })
+  
+      return this.http.put(`${environment.apiUrl}/updateprofile`,
+        {name: name, mobile:mobile, homestate:homestate, gender : gender, cast: cast, city:city, physical_status:physical_status, email:email, score:neet}, {headers: headers});
+  
+    }
+  
+    // signup methods --
+  
+    sendotp(mobile : string){
+  
+      console.log(mobile);
+      return this.http.post(`${environment.apiUrl}/send-otp`, {mobile : mobile});
+    }
+    
+  
+    uploadPhofilephoto(access_token : string,param: any){
+      console.log("here akky",param);
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${access_token}`
+      })
+      return this.http.post(`${environment.apiUrl}/upload-profile-photo`, {file : param},{ headers });
+    }
+  
+    loginByGoogle(user_detail:any):any {
+      console.log("before post",user_detail.email);
+      
+      let loginData : any = {};
+      loginData.email = user_detail.email;
+      loginData.name = user_detail.name;
+      loginData.id = user_detail.id;
+      loginData.provider = user_detail.provider;
+      loginData.photoUrl = user_detail.photoUrl;
+      console.log("before post",loginData);
+      return this.http.post(`${environment.apiUrl}/login-by-google`, {loginData});
+    }
+}
