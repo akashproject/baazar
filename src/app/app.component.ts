a7bef8fcea74737c1899b0033e31172bb3130c74
akashproject <akashdutta.scriptcrown@gmail.com>
2024-07-08 16:38:13 +0530

    update 2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
+import { ApiService  } from './services/api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'baazar';
+export class AppComponent implements OnInit{
+
+  allSettings :any =  (localStorage.getItem('allSettings') !== null)?JSON.parse(localStorage.getItem('allSettings') || '{}'):null;
+  constructor(private api: ApiService) {
+      
+  }
+
+
+  ngOnInit(): void {
+    this.getAllSettings()
+  }
+
+  getAllSettings(){
+    if (this.allSettings == null) {
+      this.api.get("all-settings").subscribe((data: any) => {
+         localStorage.setItem("allSettings",JSON.stringify(data))
+      });
+    }
+  }
 }
--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  allSettings :any =  (localStorage.getItem('allSettings') !== null)?JSON.parse(localStorage.getItem('allSettings') || '{}'):null;
+
+  ngOnInit(): void {
+    
+    console.log(this.allSettings);
+
+  }
 
 }
